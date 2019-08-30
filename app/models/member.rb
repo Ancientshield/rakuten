edea8222999be3bc347b7bb215efbbde03e96a88
Ancientshield <ancientsword520@gmail.com>
2019-08-30 16:26:42 +0800

    validate email

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,14 @@
 class Member < ApplicationRecord
+  validates :name, :phone, presence: true
+  validates :email, :format => /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  private
+  def name_validator
+  end
+
+  def phone_validator
+  end
+
+  def email_validator
+  end
 end
