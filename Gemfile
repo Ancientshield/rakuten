93298185a41de33c2ed730ffdedb542e31f21aae
Ancientshield <ancientsword520@gmail.com>
2019-08-30 13:27:19 +0800

    new & edit form_for create

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.0'
 
+gem 'simple_form', '~> 4.1'
 gem 'pg', '~> 1.1', '>= 1.1.4'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.0'
--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,4 +2,26 @@ class MembersController < ApplicationController
   def index
     @members = Member.all
   end
+  def new
+    @member = Member.new
+  end
+  def create
+    @member = Member.new(member_params)
+    if @member.save
+      redirect_to members_path, notice: "new member created success."
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    @member = Member.find_by(id: params[:id])
+  end
+
+
+  private
+  def member_params
+    params.require(:member).permit(:name, :phone, :email)
+  end
+
 end
